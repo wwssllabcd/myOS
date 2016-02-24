bb7ad895b0e0290b76a10143df982a24030c9ac1
EricWang <wwssllabcd@gmail.com>
2016-02-25 00:31:40 +0800

    Roll back to old Version

--- boot.s
+++ boot.s
@@ -1,136 +1,134 @@
-	.code16
-# rewrite with AT&T syntax by falcon <wuzhangjin@gmail.com> at 081012
-#
-# SYS_SIZE is the number of clicks (16 bytes) to be loaded.
-# 0x3000 is 0x30000 bytes = 196kB, more than enough for current
-# versions of linux
-#
-	.equ SYSSIZE, 0x3000
-#
-#	bootsect.s		(C) 1991 Linus Torvalds
-#
-# bootsect.s is loaded at 0x7c00 by the bios-startup routines, and moves
-# iself out of the way to address 0x90000, and jumps there.
-#
-# It then loads 'setup' directly after itself (0x90200), and the system
-# at 0x10000, using BIOS interrupts. 
-#
-# NOTE! currently system is at most 8*65536 bytes long. This should be no
-# problem, even in the future. I want to keep it simple. This 512 kB
-# kernel size should be enough, especially as this doesn't contain the
-# buffer cache as in minix
-#
-# The loader has been made as simple as possible, and continuos
-# read errors will result in a unbreakable loop. Reboot by hand. It
-# loads pretty fast by getting whole sectors at a time whenever possible.
+.code16       #使用16位模式彙編, AT&T( GAS )語法
 
-	.global _start, begtext, begdata, begbss, endtext, enddata, endbss
-	.text
-	begtext:
-	.data
-	begdata:
-	.bss
-	begbss:
-	.text
+.equ SETUPLEN, 4		# nr of setup-sectors，boot相關的使用4個sector
+.equ BOOTSEG, 0x07C0		# original address of boot-sector
+.equ INITSEG, 0x9000		# we move boot here - out of the way
+.equ SETUPSEG, 0x9020
 
-	.equ SETUPLEN, 4		# nr of setup-sectors
-	.equ BOOTSEG, 0x07c0		# original address of boot-sector
-	.equ INITSEG, 0x9000		# we move boot here - out of the way
-	.equ SETUPSEG, 0x9020		# setup starts here
-	.equ SYSSEG, 0x1000		# system loaded at 0x10000 (65536).
-	.equ ENDSEG, SYSSEG + SYSSIZE	# where to stop loading
+.equ SYSSEG, 0x1000		    # system loaded at 0x10000 (65536).
+.equ SYSSIZE, 0x3000
+.equ ENDSEG, SYSSEG + SYSSIZE	# where to stop loading
 
-# ROOT_DEV:	0x000 - same type of floppy as boot.
-#		0x301 - first partition on first drive etc
-	.equ ROOT_DEV, 0x301       # 這邊代表要讀取那個裝置，0x301是讀取/dev/hda1，也可以替換其他裝置
+.equ ROOT_DEV, 0x301        # 這邊代表要讀取那個裝置，0x301是讀取/dev/hda1，也可以替換其他裝置
 
-	# ljmp CS,IP: 因為目前就是在07C0的位置，所以若要跳到 _start這個位置，而這個位置就是0x7C00 + _start 的 offset 的地方
-	# 又因為CS是少一個0，所以這邊要填0x7C00變成0x07C0
-	# 這邊一個是CS(BOOTSEG, 0x07c0), 一個是EIP(_start, 這邊就是_start的offset)，因為目前就是在0x07C0的位置
-	ljmp    $BOOTSEG, $_start
+.equ DX_SAVE, 0x07C0
+
+#保留給游標 X,Y 使用DX register
+dx_reg: .word 0x0000
+
+
+
+	#ljmp CS,IP: 因為目前就是在07C0的位置，所以若要跳到 _start這個位置，而這個位置就是0x7C00 + _start 的 offset 的地方
+	#又因為CS是少一個0，所以這邊要填0x7C00變成0x07C0
+	ljmp $BOOTSEG, $_start
+
 _start:
-	# 接下來就是把自己移動到 0x90000 的位置，為何要移動，是因為setup.S將參數表保存到那裡而預留空間
-	mov	$BOOTSEG, %ax          # 把0x07C0 設給 ds, 把0x9000設給 es, 等下copy 的時候，會用到源地址的組合是 [ds:si], 相應的目標地址為[es:di]
+	#CS 因為ljmp的關係，現在應該是0x07C0，他把所有的segment(ds,es)都設成CS所在的區段
+	mov	%cs,%ax
+	mov	%ax,%ds
+	mov	%ax,%es
+	mov	$0xFF00, %sp
+
+MovDataTo9k:
+
+	# %bp is string位置, %cx為長度
+	mov	$str_0, %bp
+	mov	$0x1, %cx
+	mov	$0x0, %dx
+	call DispStr
+
+	# 從0x9000，也就是9w的地方開始，其原因是因為0~8FFFF的地方是之後系統使用
+	# 所以init從9w開始就不會影響到系統，此處也是被規劃成RAM的位置
+	mov	$BOOTSEG, %ax
 	mov	%ax, %ds
-	mov	$INITSEG, %ax          # 利用ax來設定es，雖然是9w，但實際上設定eS必須設定為9k，這樣CS:IP才會變成9w
+	mov	$INITSEG, %ax
 	mov	%ax, %es
-	mov	$256, %cx              # cx register通常拿來當counter, 也就是for loop中的 i，這邊打算要copy256個word到某地
-	sub	%si, %si               # sub 是暫存器相減，這裡的意思同 xor %si, %si, 也就是si相減，清成0
+	mov	$256, %cx        # cx = counter
+	sub	%si, %si         # sub 是暫存器相減，清成0
 	sub	%di, %di
-	rep	                       # rep為repeat指令，他會根據CX值，當作repeat的次數
-	movsw                      # movw AT&T 語法 是隱含操作數的，從 [ds:si]->[es:di], Intel語法好像是 movsw, 也就是copy data從 0x07C0到0x9000，相較於MOVSB，MOVSW是以WORD為單位
-	ljmp	$INITSEG, $go      # 跳到 CS:IP = INITSEG:go 處，也就是不但把自己移到0x9000之外，也把目前執行處，從0x7Cxx移到0x90XX之後，接續著做下去
+	rep
+	movsw                # 從 [ds:si]->[es:di]
 
-go:	mov	%cs, %ax               # copy完成後，把ds, es, ss 重設成 cs (0x9000)
+	# show msg
+	mov	$str_1, %bp
+	mov	$0x1, %cx
+	mov	$0x0001, %dx
+	call DispStr
+
+
+jumpTo9000:
+	ljmp	$INITSEG, $go_9000
+
+go_9000:
+	mov	%cs, %ax
 	mov	%ax, %ds
 	mov	%ax, %es
-# put stack at 0x9ff00.
 	mov	%ax, %ss
-	mov	$0xFF00, %sp		   # arbitrary value >>512
+	mov	$0xFF00, %sp   # 9FF00
 
-# load the setup-sectors directly after the bootblock.
-# Note that 'es' is already set up.
-# 準備讀取sec 2 出來，此時sec 2 是放置 setup.S 的 code
-# 使用int 13時，放置 data的地方是以 ES:BX 來代表
-load_setup:
-	mov	$0x0000, %dx		# drive 0, head 0           # DH=head, DL=drive
-	mov	$0x0002, %cx		# sector 2, track 0         # CH=track, CL=startSector, 把 sec 2 讀出來(chs mode 是以sec 1 作為開始)
-	mov	$0x0200, %bx		# address = 512, in INITSEG # address = ES*0x10 + BX, ES:BX 指向該service要存放在哪,由此可知這邊是要把data讀出來放到0x90200的位置
-	.equ    AX, 0x0200+SETUPLEN                         # 這邊的值為0x0204, AH是0x02號 service (把data放到ram), AL 為讀幾個 sector
-	mov     $AX, %ax		# service 2, nr of sectors
-	int	$0x13			    # read it                   # INT 13H/AH=02H：讀取磁區
-	jnc	ok_load_setup		# ok - continue
-	
-	#下reset disk cmd 之後，在跳到最前面，看起來沒有退路，讀到成功為止，否則就是死循環
+ec_load_setup:
+	# show msg
+	mov	$str_2, %bp
+	mov	$0x1, %cx
+	mov	$0x0002, %dx
+	call DispStr
+
+    #不能直接指定數值給es, 否則編譯器會報錯
+    # ES:BX是存放位置
+    mov	$INITSEG, %ax
+    mov %ax, %es
+
+	mov	$0x0200, %bx        # addr=512, ES:BX=target, 由此可知這邊是要把data讀出來放到0x90200的位置
+	mov	$0x0000, %dx        # drive 0, head 0
+	mov	$0x0002, %cx        # sector 2, track 0, 把 sec 2 讀出來(chs mode 是以sec 1 作為開始)
+	.equ    AX, 0x0200+SETUPLEN
+	mov     $AX, %ax	    # 這邊的值為0x0204, AH是0x02號 service (把data放到ram), AL 為讀幾個 sector，代表讀4個sector出來
+	int	$0x13		        # read it, INT 13H/AH=02H：讀取磁區
+
+	jnc	ec_ok_load_setup  	    # ok - continue
+
+	# if result = bad, 下reset disk cmd 之後，在跳到最前面，看起來沒有退路，讀到成功為止，否則就是死循環
 	mov	$0x0000, %dx
 	mov	$0x0000, %ax		# reset the diskette
 	int	$0x13
-	jmp	load_setup
 
-ok_load_setup:
+ec_ok_load_setup:
+	# set msg x,y
+	# show msg
+	mov	$str_3, %bp
+	mov	$0x1, %cx
+	mov	$0x0003, %dx
+	call DispStr
+
+ec_load_cy_sec:
 
 # Get disk drive parameters, specifically nr of sectors/track
 # 把CX的內容(也就是cy與 sec per track)存在cs:sectors+0 中，sector 為一位置，在最下面有定義
 # 要知道目前硬碟的 sector 位在哪，才知道還剩多少沒有讀取
 	mov	$0x00, %dl          # DL= drive No, int 13, AH=8 = get param
 	mov	$0x0800, %ax		# AH=8 is get drive parameters
-	int	$0x13
+	int	$0x13               # INT 13h AH=08h: Read Drive Parameters, return dx,cx
 
-	mov	$0x00, %ch          # 把 cylinder 設為0(應該是用不到的關係)
-	#seg cs
-	mov	%cx, %cs:sectors+0  # %cs means sectors is in %cs , 值為0x12
-
-	mov	$INITSEG, %ax       # INITSEG的值為0x9000，因為讀取磁盤的參數會改掉ES，所以重設
-	mov	%ax, %es
-
-# Print some inane message
-	# 使用int10, AH=3 先讀取 cursor 位置後，再以目前位置寫入, DX ＝ 圖形坐標列(X)、行(Y)
-	mov	$0x03, %ah		# read cursor pos
-	xor	%bh, %bh        # BH清成0
-	int	$0x10           # AH=03H/INT 10H ，DH = Row, DL = Column
-	
-	mov	$24, %cx
-	mov	$0x0007, %bx	# page 0, attribute 7 (normal)
-	#lea	msg1, %bp
-	mov $msg1, %bp      # ES:BP = Offset of string, 顯示load system..
-	mov	$0x1301, %ax		# write string, move cursor  # AH=13:在Teletype模式下顯示字符串, AL＝像素值
-	int	$0x10
+	mov	$0x00, %ch          # clear high byte
+	mov	%cx, %cs:sectors+0  # %cs means sectors per track, save SecPerTrack，這裡讀出來的是SecPerTrack = 0x12
 
 # ok, we've written the message, now
 # we want to load the system (at 0x10000)
+	mov	$SYSSEG, %ax        # SYSSEG=0x1000,  system loaded at 0x10000 (65536)64k.
+	mov	%ax, %es		    # segment of 0x010000
 
-	mov	$SYSSEG, %ax    # SYSSEG=0x1000,  system loaded at 0x10000 (65536)64k.
-	mov	%ax, %es		# segment of 0x010000
-	call	read_it     # read_it 在下面
+	call	read_it         # read_it 在下面
 	call	kill_motor
 
-# After that we check which root-device to use. If the device is
-# defined (#= 0), nothing is done and the given device is used.
-# Otherwise, either /dev/PS0 (2,28) or /dev/at0 (2,8), depending
-# on the number of sectors that the BIOS reports currently.
-
+	# After that we check which root-device to use. If the device is
+	# defined (#= 0), nothing is done and the given device is used.
+	# Otherwise, either /dev/PS0 (2,28) or /dev/at0 (2,8), depending
+	# on the number of sectors that the BIOS reports currently.
 	#seg cs
+
 	mov	%cs:root_dev+0, %ax  # root_dev 使用code 的方式 hard code, 預設值為 0x301
+
+
 	cmp	$0, %ax              # 檢查該直是否為 0   
 	jne	root_defined         # jne (jump not equal) 	不等於則轉移 	檢查 zf=0
 	#seg cs
@@ -151,6 +149,13 @@ root_defined:
 # the setup-routine loaded directly after
 # the bootblock:
 
+	# show msg
+	mov	$str_4, %bp
+	mov	$0x1, %cx
+	mov	$0x0004, %dx
+	call DispStr
+
+jump_2_90200:
 	ljmp	$SETUPSEG, $0  # 跳到0x90200，執行setup.S
 
 # This routine loads the system at address 0x10000, making sure
@@ -159,54 +164,59 @@ root_defined:
 #
 # in:	es - starting address segment (normally 0x1000)
 #
-sread:	.word 1+ SETUPLEN	# sectors read of current track
-head:	.word 0			# current head
-track:	.word 0			# current track
+sread:	.word 1+SETUPLEN	# sectors read of current track，預設值為 5(1+4)，因為前面4個是setup使用的，所以system的從sector 5號開始
+head:	.word 0			    # current head
+track:	.word 0			    # current track
 
-#要把 system從磁碟讀到0x10000的位置
+#要把 system從磁碟讀到 0x10000 的位置，不讀到 0 的位置可能是怕蓋過 ISR
+#之後要把 system 搬到 0 的位置時，是關閉 interrupt 的
 read_it:
 	mov		%es, %ax        # es已經被設定為 0x1000
 	test	$0x0fff, %ax    # 測試 es 是否為 0x1000
 die:	
-	jne 	die				# es must be at 64kB boundary(也就是es = 0x1000), 所以不對就進入死循環
-	xor 	%bx, %bx		# bx is starting address within segment ,設 bx =0 代表 ES:BX = 0x1000:0
+	jne 	die				# es must be at 64kB boundary(也就是es = 0x1000)
+	xor 	%bx, %bx		# bx 設為 0，代表資料存放位置為[ES:BX] = 0x1000:0
 rp_read:
-	mov 	%es, %ax        # 檢查目前 ES 是否已經到達 0x3000 
+	#%es為目前讀了多少資料，ENDSEG為資料的尾端
+	mov 	%es, %ax
  	cmp 	$ENDSEG, %ax	# have we loaded all yet? # sys_start = 0x1000, sts_end = 0x3000 ，所以大小為100個sector?
-	jb		ok1_read        # 如果已經到底了，就 return
+	jb		ok1_read        #
 	ret
 ok1_read:
-	#seg cs
-	mov		%cs:sectors+0, %ax   # 讀取之前的 secPerTrack/cylinder, 這邊是0x12
-	sub		sread, %ax           # ax = ax - sread , 也就是 0x12 - 5 = 0x0d, 還剩 0x0d 個 sec 要讀
+	#這邊主要處理要讀多少sector, 還有讀完之後的offset為多少
+	mov		%cs:sectors+0, %ax   # 讀取之前的 secPerTrack，值為12
+	sub		sread, %ax       # ax = ax - sread, cs:sread = 5, 所以這邊ax會變成0x0d，還有0x0d個sector要讀，這邊先設好AL(要讀多少sector)
 	mov		%ax, %cx
-	shl		$9, %cx              # 也就是 cx * 512, 代表這次打算要讀多少sector, 為 0x0d*0x200 = 0x1a00
-	add		%bx, %cx			 # 因為 bx 為int 13 的 ES:BX, 所以 cx = bx+cx 是利用cx來測試這次的讀取動作有沒有 overflow
-	jnc 	ok2_read             # cf =0, 如果沒有 overflow, 就可以執行這次的read, 否則要調整 read 的 sector
+
+	shl		$9, %cx              # cx = cx*512 (這次讀多少 byte), shl = 左移9個bit
+	add		%bx, %cx             # cx=cx+bx, cx代表這次讀了之後，offset會跑到哪裡(可能是之後要設回bx用)
+	jnc 	ok2_read
 	je 		ok2_read
-	xor 	%ax, %ax             # 這裡看起來是處理 BX已經過頭的地方
-	sub 	%bx, %ax             # 現在的bx 就是代表 overflow 多少個byte, 減去0，代表要讀多少byte出來
-	shr 	$9, %ax              # ax 除512, 則代表要讀多少個sector
+	xor 	%ax, %ax
+	sub 	%bx, %ax
+	shr 	$9, %ax
 ok2_read:
-	call 	read_track       # 要跳進去之前，AL要先設定好，代表本次要讀多少sector
-	mov 	%ax, %cx         # ax = 這次操作所讀的sector數, 即 0x0d
-	add 	sread, %ax       # ax = 已經讀多少sector 0x05 +0x0d => %ax = 0x12
-	#seg cs
-	cmp 	%cs:sectors+0, %ax   # 比較"secPerTrack"與"目前讀取多少sector" ，如果一樣，就往下執行
-	jne 	ok3_read
+	call 	read_track
+	mov 	%ax, %cx             # 本次讀多少sector 備份到 cx中，%ax = 0x000d, 第一次是讀  0xd個sector (0x12 - 5 )
+	add 	sread, %ax       # ax = ax + sread , 應該會讀到sector 的尾巴，也就是0x12
+	cmp 	%cs:sectors+0, %ax   # 比較ax，是否為secPerTrack(也就是判斷是否到尾巴)?如果相等，則ZF flag=1
+
+	jne 	ok3_read             # 不等於，則jmp(x86中相對應的cmd為je)
 	mov 	$1, %ax
-	sub 	head, %ax        # 看看head是否為1, ax - head = ax
-	jne 	ok4_read         # 如果head目前是0,則會跳到 ok4_read 去執行
-	incw    track            # 如果head為1 ，則 track + 1
+
+	sub 	head, %ax
+	jne 	ok4_read            # 如果cs:head==0，則jump 到 ok4_read
+	incw    track
 ok4_read:
-	mov	%ax, head      # head設為1
-	xor	%ax, %ax       # ax 設0 
+	#設定 cs:head = 1，並清除ax
+	mov	%ax, head
+	xor	%ax, %ax
 ok3_read:
-	mov	%ax, sread     # ax 若是讀為整個track的話，則為0, 否則為 %cs:sectors，即secPerTrack
-	shl	$9, %cx		   # cx 為這次讀多少sector, 為 0x0d*512
-	add	%cx, %bx       # 把目前讀到哪的offset , 重新設給 bx, 因為int 13 read 是靠 ES:BX
-	jnc	rp_read        # 如果無進位符號，則跳躍 if cf==0, then jump rp_read
-	mov	%es, %ax       # 這邊看起來是控制 ES 是否 需要 + 0x1000的地方
+	mov	%ax, sread
+	shl	$9, %cx
+	add	%cx, %bx              # 重新設定 bx(因為es:bx)到正確的offset
+	jnc	rp_read
+	mov	%es, %ax
 	add	$0x1000, %ax
 	mov	%ax, %es
 	xor	%bx, %bx
@@ -215,22 +225,34 @@ ok3_read:
 read_track:
 	push	%ax
 	push	%bx
-	push	%cx       
+	push	%cx
 	push	%dx
-	mov	track, %dx
-	mov	sread, %cx
-	inc	%cx            # CL = sector number 1-63 (bits 0-5), 這邊固定 +1, 因為sec是從1開始
-	mov	%dl, %ch
-	mov	head, %dx      # 取出 head
-	mov	%dl, %dh       # DH = head number
-	mov	$0, %dl        # DL = drive number (bit 7 set for hard disk)
-	and	$0x0100, %dx   # 做 and 運算, 結果應該是 dx=0, 這意思是，只會讀到header 1?
-	mov	$2, %ah        # int 13, AH=2 :READ SECTOR(S) INTO MEMORY, AL = number of sectors to read (must be nonzero)
-	int	$0x13          # 第一次進來的時候，AX是keep住還剩多少sec, 也就是 0x0d, 讀到 ES:BX, ES之前設過是0x1000
-	 
+
+	mov	track, %dx   # cs:track 一開始是0, so dx=0
+	mov	sread, %cx   # cx=5
+	inc	%cx              # cx=cx+1, 這動作主要是設cl, 即把 sector+1 ( chs mode 以1為起始)
+	mov	%dl, %ch         # dl 此時是 track, 設給ch
+
+	mov	head, %dx    # 取 head
+	mov	%dl, %dh		 # head 設到 %dh, DH = head number
+	mov	$0, %dl          # DL = drive number ,DriveNo=0(means Drive A)
+	and	$0x0100, %dx     # dx=dx&0x100, 跟0x100作 "&"動作, 這裡為限制 dl=0, dh不超過1
+
+	#--------------------------------
+	#INT 13h AH=02h: Read Sectors From Drive
+	# AL 	Sectors To Read Count
+	# CH 	Cylinder
+	# CL 	Sector
+	# DH 	Head
+	# DL 	Drive
+	# ES:BX 	Buffer Address Pointer
+	mov	$2, %ah	         # int 13 (AH=2) , es:bx , AL = number of sectors to read (must be nonzero)
+	int	$0x13
+
 	jc	bad_rt
+
 	pop	%dx
-	pop	%cx            # cx 為，這次的op, 到底讀出多少byte 
+	pop	%cx
 	pop	%bx
 	pop	%ax
 	ret
@@ -256,23 +278,53 @@ kill_motor:
 	pop		%dx
 	ret
 
+
+
+
+#=========================
+#string addr= ES:BP = 串地址
+#strLen = %cx
+#screen X = %dl
+#screen Y = %dH
+DispStr:
+
+	push %ax
+	push %es
+
+	#要把ES重設, 因為如果ES設離開了9000 or 7C0，string 會出不來，特別是經過load system那段組語，會改變ES的值
+	mov	$BOOTSEG, %ax
+    mov %ax, %es
+
+	mov $0x1301, %ax      # AH = 13, AL = 01h
+	mov $0x000C, %bx      # 頁號為0(BH = 0) 黑底紅字(BL = 0Ch,高亮)
+	int $0x10             # 10h 號中斷
+
+	pop %es
+	pop %ax
+
+	ret
 sectors:
 	.word 0
 
-msg1:
-	.byte 13,10
-	.ascii "Loading system ..."
-	.byte 13,10,13,10
+str_0:      .ascii "0"
+str_1:      .ascii "1"
+str_2:      .ascii "2"
+str_3:      .ascii "3"
+str_4:      .ascii "4"
+str_5:      .ascii "5"
+str_6:      .ascii "6"
 
-	.org 508
+.org 508
 root_dev:
 	.word ROOT_DEV
 boot_flag:
 	.word 0xAA55
-	
+
 	.text
 	endtext:
 	.data
 	enddata:
 	.bss
 	endbss:
+
+
--- setup.s
+++ setup.s
@@ -1,86 +1,95 @@
-	.code16
-# rewrite with AT&T syntax by falcon <wuzhangjin@gmail.com> at 081012
-#
-#	setup.s		(C) 1991 Linus Torvalds
-#
-# setup.s is responsible for getting the system data from the BIOS,
-# and putting them into the appropriate places in system memory.
-# both setup.s and system has been loaded by the bootblock.
-#
-# This code asks the bios for memory/disk/other parameters, and
-# puts them in a "safe" place: 0x90000-0x901FF, ie where the
-# boot-block used to be. It is then up to the protected mode
-# system to read them from there before the area is overwritten
-# for buffer-blocks.
-#
+.code16
+.equ INITSEG, 0x9000	# we move boot here - out of the way
+.equ SYSSEG, 0x1000	    # system loaded at 0x10000 (65536).
+.equ SETUPSEG, 0x9020	# this is the current segment
 
-# NOTE! These had better be the same as in bootsect.s!
-
-	.equ INITSEG, 0x9000	# we move boot here - out of the way
-	.equ SYSSEG, 0x1000	# system loaded at 0x10000 (65536).
-	.equ SETUPSEG, 0x9020	# this is the current segment
-
-	.global _start, begtext, begdata, begbss, endtext, enddata, endbss
-	.text
-	begtext:
-	.data
-	begdata:
-	.bss
-	begbss:
-	.text
-
-	ljmp $SETUPSEG, $_start	 # setup.s是放在 0x90200的位置
 _start:
+	ljmp $SETUPSEG, $_start_s	 # setup.s是放在 0x90200的位置
 
-# ok, the read went well so we get current cursor position and save it for
-# posterity.
+# string should be here
+str_S0:      .ascii "0"
+str_S1:      .ascii "1"
+str_S2:      .ascii "2"
+str_S3:      .ascii "3"
 
+DispStr:
+	push %ax
+	push %es
+
+	# reset es to SETUPSEG
+	mov	$SETUPSEG, %ax
+    mov %ax, %es
+
+	mov $0x1301, %ax      # AH = 13, AL = 01h
+	mov $0x000C, %bx      # 頁號為0(BH = 0) 黑底紅字(BL = 0Ch,高亮)
+	int $0x10             # 10h 號中斷
+
+	pop %es
+	pop %ax
+	ret
+
+_start_s:
+	#===================
+	mov	$0x91000, %sp
+
+	mov	$str_S0, %bp
+	mov	$0x1, %cx
+	mov	$0x0100, %dx
+	call DispStr
+
+	#init all segment
 	mov	$INITSEG, %ax	# this is done in bootsect already, but...
 	mov	%ax, %ds
+
 	mov	$0x03, %ah	# read cursor pos
 	xor	%bh, %bh
 	int	$0x10		# save it in known place, con_init fetches # 使用int10, AH=3 先讀取 cursor 位置
-	mov	%dx, %ds:0	# it from 0x90000.                         # 把 DX(Y軸) 存入 0x90000 的位置
-# Get memory size (extended mem, kB)
+	mov	%dx, %ds:0	# it from 0x90000.                         # 把 DX(XY軸) 存入 0x90000 的位置
 
-	mov	$0x88, %ah      # int 15, AH = 0x88, 取得記憶體大小
+# Get memory size (extended mem, kB)
+	mov	$0x88, %ah      # int 15, AH = 0x88, 取得記憶體大小，為0x00FC
 	int	$0x15
 	mov	%ax, %ds:2      # 0x90002
 
 # Get video-card data:
-
 	mov	$0x0f, %ah      # int 10, AH= 0x0F = Get current video mode, return AL = Video Mode
 	int	$0x10
-	mov	%bx, %ds:4	# bh = display page
-	mov	%ax, %ds:6	# al = video mode, ah = window width
+	mov	%bx, %ds:4	# bh = display page, val=0x0C00
+	mov	%ax, %ds:6	# al = video mode, ah = window width, val=0x0350
 
 # check for EGA/VGA and some config parameters
 
 	mov	$0x12, %ah
 	mov	$0x10, %bl
 	int	$0x10
-	mov	%ax, %ds:8
-	mov	%bx, %ds:10
-	mov	%cx, %ds:12
+	mov	%ax, %ds:8      # val=0x0312
+	mov	%bx, %ds:10     # val=0x0300
+	mov	%cx, %ds:12     # val=0x0900
 
+	#===================
+	mov	$str_S1, %bp
+	mov	$0x1, %cx
+	mov	$0x0101, %dx
+	call DispStr
+
+read_HDA0:
 # Get hd0 data
 
 	# 這邊取中斷向量編號 0x41 號的值, 因為一個中斷佔4個byte, 所以No 41h所在的offset其實就是41*4 = 0x104h
-	# 其實 記憶體 0 ~ 0x3FF(共400H) 提供給中斷向量表使用,可搜尋"BIOS中斷向量表 （轉）"關鍵字
-	# 使用 qemu的 seaBios會取出 0x9FC0:003D, 若沒有掛HD0會取出 0xF000:FF53
+	# 其實 記憶體 0 ~ 0x3FF(共400H) 提供給中斷向量表使用,可搜尋關鍵字 "BIOS中斷向量表"
 	
 	mov	$0x0000, %ax
 	mov	%ax, %ds
-	lds	%ds:4*0x41, %si   # LDS = load DS, 把這個 string( ds:4*41) 的位置，設給 ds:si ,0x104處存儲的，是硬盤參數表的地址，不是硬盤參數表的內容
+	lds	%ds:4*0x41, %si
+
 	mov	$INITSEG, %ax
 	mov	%ax, %es
 	mov	$0x0080, %di   # 目的地ES:DI = 0x90080
-	mov	$0x10, %cx     # cx register通常拿來當counter, 也就是for loop中的 i
+	mov	$0x10, %cx     # cx register通常拿來當cnt, 也就是 for loop中的 i
 	rep
-	movsb                  # 從 DS:SI->ES:DI
+	movsb              # 從 DS:SI->ES:DI，一次一個BYTE，共 copy 16 個 BYTE
 
 # Get hd1 data
-
 	mov	$0x0000, %ax
 	mov	%ax, %ds
 	lds	%ds:4*0x46, %si
@@ -91,14 +100,18 @@ _start:
 	rep
 	movsb
 
-# Check that there IS a hd1 :-)
-
-	mov	$0x01500, %ax   # int 15, AH = 15 => Read Drive Type
+	# Check that there IS a hd1 :-)
+	# int 15, AH = 15 => Read Drive Type
+	mov	$0x01500, %ax
 	mov	$0x81, %dl      # DL = drive number (0=A:, 1=2nd floppy, 80h=drive 0, 81h=drive 1)
 	int	$0x13
+
+
+	# res => CF = 0
 	jc	no_disk1        # CF = 0 if successful, CF = 1 if error
-	cmp	$3, %ah         # AH = 03, fixed disk present
+	cmp	$3, %ah         # AH = 03, 代表為HD, 01 = diskette, no change detection present(這邊的AH=0x01)
 	je	is_disk1
+
 no_disk1:
 	mov	$INITSEG, %ax
 	mov	%ax, %es
@@ -106,34 +119,47 @@ no_disk1:
 	mov	$0x10, %cx      # 當作 stosb 的 count
 	mov	$0x00, %ax      # 準備要把ES:DI存成0
 	rep
-	stosb   # STOSB 是將 AX 存放到 ES:DI 的位置，並根據 DF 來遞增 (DF = 0) 或遞減 (DF = 1) DI，以指向下一個位置
+	stosb               # STOSB 是將 AX 存放到 ES:DI 的位置，並根據 DF 來遞增 (DF = 0) 或遞減 (DF = 1) DI，以指向下一個位置
+
 is_disk1:
+	#===================
+	mov	$str_S2, %bp
+	mov	$0x1, %cx
+	mov	$0x0102, %dx
+	call DispStr
 
 # now we want to move to protected mode ...
 
-	cli			# no interrupts allowed ! 
+	cli			      # clear interrupt, no interrupts allowed !
 
 # first we move the system to it's rightful place
 
 	mov	$0x0000, %ax
-	cld			# 'direction'=0, movs moves forward
+	cld			      # 'direction'=0, movs moves forward
+
+
+
 do_move:
-	mov	%ax, %es	  # destination segment   # ES設為0
+	mov	%ax, %es	  # ES設為0
 	add	$0x1000, %ax  # ax從 0x1000開始，到0x9000結束，每次加0x1000
 	cmp	$0x9000, %ax  # 是否到9000的位置了？
 	jz	end_move
+
 	mov	%ax, %ds	  # source segment
-	sub	%di, %di
+	mov $0x20, %cx    # cx當作cnt, 以word方式來移動，也就是一次移動0x1w byte，到9w為止
+	sub	%di, %di      # clear di, si to 0
 	sub	%si, %si
-	mov $0x8000, %cx  	# cx當作cnt, 以word方式來移動，也就是一次移動0x10000，到9w為止
 	rep
-	movsw             	# 從 [ds:si]->[es:di] ，看來有點像是把 0x10000 ~ 0x90000 移到 0x00000 ~ 0x80000, 也就是說內核大小不會超過512k
+	movsw             # 從 [ds:si]->[es:di] ，把 0x1w ~ 0x9w 移到 0x0 ~ 0x8w, 平移1wh, 也就是說內核大小不會超過512k
+
+	# 做完此動作後不能使用任何int，包括顯示用的 int 10，因為ISR被蓋過去了
+	# 32保護模式的ISR會被idt取代
 	jmp	do_move
 
 # then we load the segment descriptors
 
 end_move:
-	mov	$SETUPSEG, %ax	# right, forgot this at first. didn't work :-)  //0x9020
+	mov	$SETUPSEG, %ax	# right, forgot this at first. didn't work :-)  #0x9020
 	mov	%ax, %ds        # ds 設為 0x9020
 	lidt	idt_48		# load idt with 0,0
 	lgdt	gdt_48		# load gdt with whatever appropriate
@@ -159,62 +185,31 @@ end_move:
 # which is used for the internal hardware interrupts as well. We just
 # have to reprogram the 8259's, and it isn't fun.
 
-	# 設定 兩顆 PIC, 雖然每個 PIC 只使用兩個 I/O Port,
-	# 第一顆PIC的4個register位置在  0x20(ICW1), 0x21(ICW2~4), 第二顆在0xA0 ~ 0xA1位置
-	# 這邊有個規則就是， 0x21 若循序寫入的話，會寫到ICW2, ICW3, ICW4, OCW1 (注意，只有兩個記憶體位置)
-	
 	mov	$0x11, %al		# initialization sequence(ICW1)
-						# ICW4 needed(1),CASCADE mode,Level-triggered
-	# ( 把 Master PIC的ICW1 register 填成 0x11, 而ICW1的意義如下
-	# Bit0: 0: 選擇要不要使用ICW4, 1: 選擇使用ICW4, 
-	# Bit1: 0: 串接 mode, 1: 單顆mode, 
-	# Bit3: 0: 邊緣觸發, 1: 準位觸發 
-	# Bit4: 0: alway fix 1
-	
-	out	%al, $0x20		     # send it to 8259A-1
-	.word	0x00eb,0x00eb	 # jmp $+2, jmp $+2
-	
-	# 設定第二顆(也就是 slave PIC, 其第二顆的ICW1 位置在0xA0)
-	out	%al, $0xA0		     # and to 8259A-2
+					# ICW4 needed(1),CASCADE mode,Level-triggered
+	out	%al, $0x20		# send it to 8259A-1
+	.word	0x00eb,0x00eb		# jmp $+2, jmp $+2
+	out	%al, $0xA0		# and to 8259A-2
 	.word	0x00eb,0x00eb
-	
-	#把 0x20這個值，設給PIC0-ICW2，代表PIC-0的中斷，是由編號 0x20起跳
 	mov	$0x20, %al		# start of hardware int's (0x20)(ICW2)
 	out	%al, $0x21		# from 0x20-0x27
 	.word	0x00eb,0x00eb
-	
-	#把 0x28這個值，設給PIC1-ICW2，代表PIC-1的中斷，是由編號 0x28起跳
 	mov	$0x28, %al		# start of hardware int's 2 (0x28)
 	out	%al, $0xA1		# from 0x28-0x2F
 	.word	0x00eb,0x00eb		#               IR 7654 3210
-	
-	#設定 PIC0 為master, 其接到 slave的腳為 IRQ2(也就是第3根腳)
 	mov	$0x04, %al		# 8259-1 is master(0000 0100) --\
 	out	%al, $0x21		#				|
 	.word	0x00eb,0x00eb		#			 INT	/
-	
-	# 設定slave 是被接到 master 那個 IRQ號碼上，這邊是 IRQ2
 	mov	$0x02, %al		# 8259-2 is slave(       010 --> 2)
 	out	%al, $0xA1
 	.word	0x00eb,0x00eb
-	
-	# 設定 ICW4, 定義如下
-	# Bit0: 0: 表示要給8085使用, 1: 表示要給x86使用
-	# Bit1: Auto EOI 0: non auto , 1: auto
-	# Bit2: 緩衝區下的主(1)與僕(0)
-	# Bit3: 使用buffer(1)
-	# Bit4: 特定(1)與非特定(0)全巢模式
 	mov	$0x01, %al		# 8086 mode for both
 	out	%al, $0x21
 	.word	0x00eb,0x00eb
-	
 	out	%al, $0xA1
 	.word	0x00eb,0x00eb
-	
-	//寫入OCW1，這邊代表關掉 8 個 interrupt
 	mov	$0xFF, %al		# mask off all interrupts for now
 	out	%al, $0x21
-
 	.word	0x00eb,0x00eb
 	out	%al, $0xA1
 
@@ -232,9 +227,11 @@ end_move:
 	mov	%cr0, %eax	# get machine status(cr0|MSW)	
 	bts	$0, %eax	# turn on the PE-bit 
 	mov	%eax, %cr0	# protection enabled
-				
-				# segment-descriptor        (INDEX:TI:RPL)
+
+jmp2Protect:
+	# segment-descriptor        (INDEX:TI:RPL)
 	.equ	sel_cs0, 0x0008 # select for code segment 0 (  001:0 :00) 
+	# seg = 8 ，就是段 1的位置
 	ljmp	$sel_cs0, $0	# jmp offset 0 of code segment 0 in gdt
 
 # This routine checks that the keyboard command queue is empty
@@ -247,23 +244,25 @@ empty_8042:
 	jnz	empty_8042	# yes - loop
 	ret
 
-gdt:    # gdt table 目前設定如下, 每段為8個byte, 例如載入第一段，就是設 CS = 8, 而bit1~bit3拿來當作 flag
-	//段0 = dummy
+gdt:    # gdt table 目前設定如下, 每段為8個byte, 例如載入第一段，就是設 cS = 8, 而bit1~bit3拿來當作 flag
+	#段0 = dummy
 	.word	0,0,0,0		# dummy
 
-	//段1，也就是 CS=8 的意思，這邊又設定了 ram 0 為 code 的起始點，所以 ljmp $8, $0的意思就是執行 ram 0那邊的指令
+	# 段1，這裡就是offset 8, code 的gdt,
+	# 0x00C09A00: 一小部分的基底位置 + 權限，這邊設定是可讀寫的代碼段
+	# 0x000007FF: 段限長為8MB，基底位置為0
 	.word	0x07FF		# 8Mb - limit=2047 (2048*4096=8Mb)
 	.word	0x0000		# base address=0
 	.word	0x9A00		# code read/exec
 	.word	0x00C0		# granularity=4096, 386
 
-	//段2
-	.word	0x07FF		# 8Mb - limit=2047 (2048*4096=8Mb)
+	#段2, 0x10
+	.word	0xffFF		# 8Mb - limit=2047 (2048*4096=8Mb)
 	.word	0x0000		# base address=0
 	.word	0x9200		# data read/write
 	.word	0x00C0		# granularity=4096, 386
 
-	//段3, 0x18, Graphic Card
+	#段3, 0x18, Graphic Card
 	.word	0xFFFF		# 8Mb - limit=2047 (2048*4096=8Mb)
 	.word	0x8000		# base address=0
 	.word	0x920B		# data read/write(DA_DRW)
@@ -279,7 +278,7 @@ idt_48:
 gdt_48:
 	.word	0x800			# gdt limit=2048, 256 GDT entries
 	.word   512+gdt, 0x9	# gdt base = 0X9xxxx,   # 加 512的原因是否為 0x90200 的 0x200，也就是 setup.s的位置
-							# 逗號代表連續賦值, 如果 gdt位在0ffset 0x111的位置的話，GDTR = 0009,0311,0000,0008
+							# 逗號代表連續賦值,
 	# 512+gdt is the real gdt after setup is moved to 0x9020 * 0x10
 	
 .text
@@ -288,3 +287,6 @@ endtext:
 enddata:
 .bss
 endbss:
+
+
+
