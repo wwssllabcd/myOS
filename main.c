661875c11c935dafca1ba4328ec21e1681e971c5
EricWang <wwssllabcd@gmail.com>
2016-02-25 21:30:51 +0800

    add code

--- main.c
+++ main.c
@@ -1,4 +1,12 @@
+
+
 void main(void)	
 {
-		
+    __asm__ ("movl $0x18,%eax;" \
+            "mov %ax,%gs \n " \
+            "mov $((80*3 + 0)*2),%edi \n" \
+            "mov $0x0C,%ah \n" \
+            "mov $'m', %al \n" \
+            "mov %ax, %gs:(%edi) \n" \
+            );
 }
