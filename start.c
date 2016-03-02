638b062f818f711c2ea59c72a2ead75e7a164194
EricWang <wwssllabcd@gmail.com>
2016-03-02 18:14:33 +0800

    add miss file

---
+++ start.c
@@ -0,0 +1,22 @@
+#include "type.h"
+#include "const.h"
+#include "protect.h"
+
+PUBLIC u8 gdt_ptr;
+PUBLIC DESCRIPTOR gdt[GDT_SIZE];
+
+void showMsg()
+{
+    __asm__ ("movl $0x20, %eax;"
+            "mov %ax,%gs \n "
+            "mov $((80*3 + 0)*2), %edi \n"
+            "mov $0x0C, %ah \n"
+            "mov $'m', %al \n"
+            "mov %ax, %gs:(%edi) \n"
+    );
+}
+
+PUBLIC void cstart()
+{
+    showMsg();
+}
