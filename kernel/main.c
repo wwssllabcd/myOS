c788c240961f36fac87bc6454ed4fa5f0ae3adaf
EricWang <wwssllabcd@gmail.com>
2016-03-08 15:51:38 +0800

    add printMem

--- kernel/main.c
+++ kernel/main.c
@@ -28,6 +28,24 @@ void set_gdt(u8 itemNum, u16 data_0, u16 data_1, u16 data_2, u16 data_3)
     p_des->base_high = data_3>>16;
 }
 
+void printMem(u32 addr, u32 len)
+{
+    u32 end = addr+len;
+    u32 i;
+    disp_str("\nMEM=");
+    disp_int(addr);
+    for(i=0; i<len; i++){
+        if((i & 0xF) == 0){
+            disp_str("\n");
+            disp_int(addr+i);
+            disp_str("|");
+        }
+        u8 tmp = (*(u8*)(addr+i));
+        disp_u8(tmp);
+        disp_str(",");
+    }
+}
+
 void kernel_main(void)
 {
     PROCESS* p_proc = proc_table;
@@ -44,6 +62,8 @@ void kernel_main(void)
 
     memcpy(&p_proc->ldts[1], &gdt[SELECTOR_KERNEL_DS >> 3], sizeof(DESCRIPTOR));
     p_proc->ldts[1].attr1 = DA_DRW | PRIVILEGE_TASK << 5;   // change the DPL
+
+    printMem( (u32)(&gdt), 0x20);
 
     disp_str("\nKernel_main");
 
--- lib/klib.c
+++ lib/klib.c
@@ -1,4 +1,5 @@
 #include "const.h"
+#include "type.h"
 
 PUBLIC char * itoa(char* str, int num){
     char *p = str;
@@ -28,9 +29,46 @@ PUBLIC char * itoa(char* str, int num){
     return str;
 }
 
+PUBLIC char * itob(char* str, u8 num){
+    char *p = str;
+    char ch;
+    int i;
+    int flag=1;
+
+//    *p++ = '0';
+//    *p++ = 'x';
+
+    if(num==0){
+        *p++ = '0';
+        *p++ = '0';
+    }else{
+        for(i=4; i>=0; i-=4){
+            ch = (num>>i) & 0xF;
+            if( flag || (ch>0) ){
+                flag = 1;
+                ch += '0';
+                if( ch > '9'){
+                    ch += 7; // move to ascii "A~F"
+                }
+                *p++ = ch;
+            }
+        }
+    }
+    *p=0;
+    return str;
+}
+
+
 PUBLIC void disp_str(char* str)
 {
     disp_color_str(str, 0x0F);
+}
+
+PUBLIC void disp_u8(u8 input)
+{
+    char output[4];
+    itob(output, input);
+    disp_str(output);
 }
 
 
@@ -39,7 +77,6 @@ PUBLIC void disp_int(int input)
     char output[16];
     itoa(output, input);
     disp_str(output);
-
 }
 
 PUBLIC void delay(int time)
