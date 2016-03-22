229ae281c3d22f54749926ea7b1f1a2eea724334
EricWang <wwssllabcd@gmail.com>
2016-03-22 16:51:44 +0800

    add some unused code

--- kernel/proc.c
+++ kernel/proc.c
@@ -41,3 +41,55 @@ PUBLIC int sys_get_ticks()
 	return ticks;
 }
 
+PUBLIC int ldt_seg_linear(struct proc* p, int idx)
+{
+    struct descriptor * d = &p->ldts[idx];
+
+    return d->base_high << 24 | d->base_mid << 16 | d->base_low;
+}
+
+PUBLIC void* va2la(int pid, void* va)
+{
+    struct proc* p = &proc_table[pid];
+
+    u32 seg_base = ldt_seg_linear(p, INDEX_LDT_RW);
+    u32 la = seg_base + (u32)va;
+
+    if (pid < NR_TASKS + NR_PROCS) {
+        assert(la == (u32)va);
+    }
+
+    return (void*)la;
+}
+
+/*****************************************************************************
+ *                                block
+ *****************************************************************************/
+/**
+ * <Ring 0> This routine is called after `p_flags' has been set (!= 0), it
+ * calls `schedule()' to choose another proc as the `proc_ready'.
+ *
+ * @attention This routine does not change `p_flags'. Make sure the `p_flags'
+ * of the proc to be blocked has been set properly.
+ *
+ * @param p The proc to be blocked.
+ *****************************************************************************/
+PRIVATE void block(struct proc* p)
+{
+    assert(p->p_flags);
+    schedule();
+}
+
+/*****************************************************************************
+ *                                unblock
+ *****************************************************************************/
+/**
+ * <Ring 0> This is a dummy routine. It does nothing actually. When it is
+ * called, the `p_flags' should have been cleared (== 0).
+ *
+ * @param p The unblocked proc.
+ *****************************************************************************/
+PRIVATE void unblock(struct proc* p)
+{
+    assert(p->p_flags == 0);
+}
