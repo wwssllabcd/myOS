6e7ba491ee1139ed529ff6349f45acac808fa6e0
EricWang <wwssllabcd@gmail.com>
2016-03-30 17:50:04 +0800

    update code

--- fs/main.c
+++ fs/main.c
@@ -33,9 +33,19 @@ PRIVATE void mkfs();
  *****************************************************************************/
 PUBLIC void task_fs()
 {
-	printl("\nTask FS begins");
-	init_fs();
-	spin("FS");
+    printl("\nTask_FS Start");
+
+    /* open the device: hard disk */
+    MESSAGE driver_msg;
+    driver_msg.type = DEV_OPEN;
+    driver_msg.DEVICE = MINOR(ROOT_DEV);
+    assert(dd_map[MAJOR(ROOT_DEV)].driver_nr != INVALID_DRIVER);
+
+    //dd_map[MAJOR(ROOT_DEV)].driver_nr = 2
+    send_recv(BOTH, dd_map[MAJOR(ROOT_DEV)].driver_nr, &driver_msg);
+
+    spin("FS");
+
 }
 
 /*****************************************************************************
@@ -43,18 +53,18 @@ PUBLIC void task_fs()
  *****************************************************************************/
 /**
  * <Ring 1> Do some preparation.
- * 
+ *
  *****************************************************************************/
 PRIVATE void init_fs()
 {
-	/* open the device: hard disk */
-	MESSAGE driver_msg;
-	driver_msg.type = DEV_OPEN;
-	driver_msg.DEVICE = MINOR(ROOT_DEV);
-	assert(dd_map[MAJOR(ROOT_DEV)].driver_nr != INVALID_DRIVER);
-	send_recv(BOTH, dd_map[MAJOR(ROOT_DEV)].driver_nr, &driver_msg);
+    /* open the device: hard disk */
+    MESSAGE driver_msg;
+    driver_msg.type = DEV_OPEN;
+    driver_msg.DEVICE = MINOR(ROOT_DEV);
+    assert(dd_map[MAJOR(ROOT_DEV)].driver_nr != INVALID_DRIVER);
+    send_recv(BOTH, dd_map[MAJOR(ROOT_DEV)].driver_nr, &driver_msg);
 
-	mkfs();
+    mkfs();
 }
 
 /*****************************************************************************
@@ -71,139 +81,139 @@ PRIVATE void init_fs()
  *****************************************************************************/
 PRIVATE void mkfs()
 {
-	MESSAGE driver_msg;
-	int i, j;
+    MESSAGE driver_msg;
+    int i, j;
 
-	int bits_per_sect = SECTOR_SIZE * 8; /* 8 bits per byte */
+    int bits_per_sect = SECTOR_SIZE * 8; /* 8 bits per byte */
 
-	/* get the geometry of ROOTDEV */
-	struct part_info geo;
-	driver_msg.type		= DEV_IOCTL;
-	driver_msg.DEVICE	= MINOR(ROOT_DEV);
-	driver_msg.REQUEST	= DIOCTL_GET_GEO;
-	driver_msg.BUF		= &geo;
-	driver_msg.PROC_NR	= TASK_FS;
-	assert(dd_map[MAJOR(ROOT_DEV)].driver_nr != INVALID_DRIVER);
-	send_recv(BOTH, dd_map[MAJOR(ROOT_DEV)].driver_nr, &driver_msg);
+    /* get the geometry of ROOTDEV */
+    struct part_info geo;
+    driver_msg.type     = DEV_IOCTL;
+    driver_msg.DEVICE   = MINOR(ROOT_DEV);
+    driver_msg.REQUEST  = DIOCTL_GET_GEO;
+    driver_msg.BUF      = &geo;
+    driver_msg.PROC_NR  = TASK_FS;
+    assert(dd_map[MAJOR(ROOT_DEV)].driver_nr != INVALID_DRIVER);
+    send_recv(BOTH, dd_map[MAJOR(ROOT_DEV)].driver_nr, &driver_msg);
 
-	printl("\ndev size: 0x%x sectors", geo.size);
+    printl("\ndev size: 0x%x sectors", geo.size);
 
-	/************************/
-	/*      super block     */
-	/************************/
-	struct super_block sb;
-	sb.magic	  = MAGIC_V1;
-	sb.nr_inodes	  = bits_per_sect;
-	sb.nr_inode_sects = sb.nr_inodes * INODE_SIZE / SECTOR_SIZE;
-	sb.nr_sects	  = geo.size; /* partition size in sector */
-	sb.nr_imap_sects  = 1;
-	sb.nr_smap_sects  = sb.nr_sects / bits_per_sect + 1;
-	sb.n_1st_sect	  = 1 + 1 +   /* boot sector & super block */
-		sb.nr_imap_sects + sb.nr_smap_sects + sb.nr_inode_sects;
-	sb.root_inode	  = ROOT_INODE;
-	sb.inode_size	  = INODE_SIZE;
-	struct inode x;
-	sb.inode_isize_off= (int)&x.i_size - (int)&x;
-	sb.inode_start_off= (int)&x.i_start_sect - (int)&x;
-	sb.dir_ent_size	  = DIR_ENTRY_SIZE;
-	struct dir_entry de;
-	sb.dir_ent_inode_off = (int)&de.inode_nr - (int)&de;
-	sb.dir_ent_fname_off = (int)&de.name - (int)&de;
+    /************************/
+    /*      super block     */
+    /************************/
+    struct super_block sb;
+    sb.magic      = MAGIC_V1;
+    sb.nr_inodes      = bits_per_sect;
+    sb.nr_inode_sects = sb.nr_inodes * INODE_SIZE / SECTOR_SIZE;
+    sb.nr_sects   = geo.size; /* partition size in sector */
+    sb.nr_imap_sects  = 1;
+    sb.nr_smap_sects  = sb.nr_sects / bits_per_sect + 1;
+    sb.n_1st_sect     = 1 + 1 +   /* boot sector & super block */
+        sb.nr_imap_sects + sb.nr_smap_sects + sb.nr_inode_sects;
+    sb.root_inode     = ROOT_INODE;
+    sb.inode_size     = INODE_SIZE;
+    struct inode x;
+    sb.inode_isize_off= (int)&x.i_size - (int)&x;
+    sb.inode_start_off= (int)&x.i_start_sect - (int)&x;
+    sb.dir_ent_size   = DIR_ENTRY_SIZE;
+    struct dir_entry de;
+    sb.dir_ent_inode_off = (int)&de.inode_nr - (int)&de;
+    sb.dir_ent_fname_off = (int)&de.name - (int)&de;
 
-	memset(fsbuf, 0x90, SECTOR_SIZE);
-	memcpy(fsbuf, &sb, SUPER_BLOCK_SIZE);
+    memset(fsbuf, 0x90, SECTOR_SIZE);
+    memcpy(fsbuf, &sb, SUPER_BLOCK_SIZE);
 
-	/* write the super block */
-	WR_SECT(ROOT_DEV, 1);
+    /* write the super block */
+    WR_SECT(ROOT_DEV, 1);
 
-	printl("\ndevbase:0x%x00, sb:0x%x00, imap:0x%x00, smap:0x%x00"
-	       "\n        inodes:0x%x00, 1st_sector:0x%x00",
-	       geo.base * 2,
-	       (geo.base + 1) * 2,
-	       (geo.base + 1 + 1) * 2,
-	       (geo.base + 1 + 1 + sb.nr_imap_sects) * 2,
-	       (geo.base + 1 + 1 + sb.nr_imap_sects + sb.nr_smap_sects) * 2,
-	       (geo.base + sb.n_1st_sect) * 2);
+    printl("\ndevbase:0x%x00, sb:0x%x00, imap:0x%x00, smap:0x%x00"
+           "\n        inodes:0x%x00, 1st_sector:0x%x00",
+           geo.base * 2,
+           (geo.base + 1) * 2,
+           (geo.base + 1 + 1) * 2,
+           (geo.base + 1 + 1 + sb.nr_imap_sects) * 2,
+           (geo.base + 1 + 1 + sb.nr_imap_sects + sb.nr_smap_sects) * 2,
+           (geo.base + sb.n_1st_sect) * 2);
 
-	/************************/
-	/*       inode map      */
-	/************************/
-	memset(fsbuf, 0, SECTOR_SIZE);
-	for (i = 0; i < (NR_CONSOLES + 2); i++)
-		fsbuf[0] |= 1 << i;
+    /************************/
+    /*       inode map      */
+    /************************/
+    memset(fsbuf, 0, SECTOR_SIZE);
+    for (i = 0; i < (NR_CONSOLES + 2); i++)
+        fsbuf[0] |= 1 << i;
 
-	assert(fsbuf[0] == 0x1F);/* 0001 1111 : 
-				  *    | ||||
-				  *    | |||`--- bit 0 : reserved
-				  *    | ||`---- bit 1 : the first inode,
-				  *    | ||              which indicates `/'
-				  *    | |`----- bit 2 : /dev_tty0
-				  *    | `------ bit 3 : /dev_tty1
-				  *    `-------- bit 4 : /dev_tty2
-				  */
-	WR_SECT(ROOT_DEV, 2);
+    assert(fsbuf[0] == 0x1F);/* 0001 1111 :
+                  *    | ||||
+                  *    | |||`--- bit 0 : reserved
+                  *    | ||`---- bit 1 : the first inode,
+                  *    | ||              which indicates `/'
+                  *    | |`----- bit 2 : /dev_tty0
+                  *    | `------ bit 3 : /dev_tty1
+                  *    `-------- bit 4 : /dev_tty2
+                  */
+    WR_SECT(ROOT_DEV, 2);
 
-	/************************/
-	/*      secter map      */
-	/************************/
-	memset(fsbuf, 0, SECTOR_SIZE);
-	int nr_sects = NR_DEFAULT_FILE_SECTS + 1;
-	/*             ~~~~~~~~~~~~~~~~~~~|~   |
-	 *                                |    `--- bit 0 is reserved
-	 *                                `-------- for `/'
-	 */
-	for (i = 0; i < nr_sects / 8; i++)
-		fsbuf[i] = 0xFF;
+    /************************/
+    /*      secter map      */
+    /************************/
+    memset(fsbuf, 0, SECTOR_SIZE);
+    int nr_sects = NR_DEFAULT_FILE_SECTS + 1;
+    /*             ~~~~~~~~~~~~~~~~~~~|~   |
+     *                                |    `--- bit 0 is reserved
+     *                                `-------- for `/'
+     */
+    for (i = 0; i < nr_sects / 8; i++)
+        fsbuf[i] = 0xFF;
 
-	for (j = 0; j < nr_sects % 8; j++)
-		fsbuf[i] |= (1 << j);
+    for (j = 0; j < nr_sects % 8; j++)
+        fsbuf[i] |= (1 << j);
 
-	WR_SECT(ROOT_DEV, 2 + sb.nr_imap_sects);
+    WR_SECT(ROOT_DEV, 2 + sb.nr_imap_sects);
 
-	/* zeromemory the rest sector-map */
-	memset(fsbuf, 0, SECTOR_SIZE);
-	for (i = 1; i < sb.nr_smap_sects; i++)
-		WR_SECT(ROOT_DEV, 2 + sb.nr_imap_sects + i);
+    /* zeromemory the rest sector-map */
+    memset(fsbuf, 0, SECTOR_SIZE);
+    for (i = 1; i < sb.nr_smap_sects; i++)
+        WR_SECT(ROOT_DEV, 2 + sb.nr_imap_sects + i);
 
-	/************************/
-	/*       inodes         */
-	/************************/
-	/* inode of `/' */
-	memset(fsbuf, 0, SECTOR_SIZE);
-	struct inode * pi = (struct inode*)fsbuf;
-	pi->i_mode = I_DIRECTORY;
-	pi->i_size = DIR_ENTRY_SIZE * 4; /* 4 files:
-					  * `.',
-					  * `dev_tty0', `dev_tty1', `dev_tty2',
-					  */
-	pi->i_start_sect = sb.n_1st_sect;
-	pi->i_nr_sects = NR_DEFAULT_FILE_SECTS;
-	/* inode of `/dev_tty0~2' */
-	for (i = 0; i < NR_CONSOLES; i++) {
-		pi = (struct inode*)(fsbuf + (INODE_SIZE * (i + 1)));
-		pi->i_mode = I_CHAR_SPECIAL;
-		pi->i_size = 0;
-		pi->i_start_sect = MAKE_DEV(DEV_CHAR_TTY, i);
-		pi->i_nr_sects = 0;
-	}
-	WR_SECT(ROOT_DEV, 2 + sb.nr_imap_sects + sb.nr_smap_sects);
+    /************************/
+    /*       inodes         */
+    /************************/
+    /* inode of `/' */
+    memset(fsbuf, 0, SECTOR_SIZE);
+    struct inode * pi = (struct inode*)fsbuf;
+    pi->i_mode = I_DIRECTORY;
+    pi->i_size = DIR_ENTRY_SIZE * 4; /* 4 files:
+                      * `.',
+                      * `dev_tty0', `dev_tty1', `dev_tty2',
+                      */
+    pi->i_start_sect = sb.n_1st_sect;
+    pi->i_nr_sects = NR_DEFAULT_FILE_SECTS;
+    /* inode of `/dev_tty0~2' */
+    for (i = 0; i < NR_CONSOLES; i++) {
+        pi = (struct inode*)(fsbuf + (INODE_SIZE * (i + 1)));
+        pi->i_mode = I_CHAR_SPECIAL;
+        pi->i_size = 0;
+        pi->i_start_sect = MAKE_DEV(DEV_CHAR_TTY, i);
+        pi->i_nr_sects = 0;
+    }
+    WR_SECT(ROOT_DEV, 2 + sb.nr_imap_sects + sb.nr_smap_sects);
 
-	/************************/
-	/*          `/'         */
-	/************************/
-	memset(fsbuf, 0, SECTOR_SIZE);
-	struct dir_entry * pde = (struct dir_entry *)fsbuf;
+    /************************/
+    /*          `/'         */
+    /************************/
+    memset(fsbuf, 0, SECTOR_SIZE);
+    struct dir_entry * pde = (struct dir_entry *)fsbuf;
 
-	pde->inode_nr = 1;
-	strcpy(pde->name, ".");
+    pde->inode_nr = 1;
+    strcpy(pde->name, ".");
 
-	/* dir entries of `/dev_tty0~2' */
-	for (i = 0; i < NR_CONSOLES; i++) {
-		pde++;
-		pde->inode_nr = i + 2; /* dev_tty0's inode_nr is 2 */
-		sprintf(pde->name, "dev_tty%d", i);
-	}
-	WR_SECT(ROOT_DEV, sb.n_1st_sect);
+    /* dir entries of `/dev_tty0~2' */
+    for (i = 0; i < NR_CONSOLES; i++) {
+        pde++;
+        pde->inode_nr = i + 2; /* dev_tty0's inode_nr is 2 */
+        sprintf(pde->name, "dev_tty%d", i);
+    }
+    WR_SECT(ROOT_DEV, sb.n_1st_sect);
 }
 
 /*****************************************************************************
@@ -211,30 +221,29 @@ PRIVATE void mkfs()
  *****************************************************************************/
 /**
  * <Ring 1> R/W a sector via messaging with the corresponding driver.
- * 
+ *
  * @param io_type  DEV_READ or DEV_WRITE
  * @param dev      device nr
  * @param pos      Byte offset from/to where to r/w.
  * @param bytes    r/w count in bytes.
  * @param proc_nr  To whom the buffer belongs.
  * @param buf      r/w buffer.
- * 
+ *
  * @return Zero if success.
  *****************************************************************************/
 PUBLIC int rw_sector(int io_type, int dev, u64 pos, int bytes, int proc_nr,
-		     void* buf)
+             void* buf)
 {
-	MESSAGE driver_msg;
+    MESSAGE driver_msg;
 
-	driver_msg.type		= io_type;
-	driver_msg.DEVICE	= MINOR(dev);
-	driver_msg.POSITION	= pos;
-	driver_msg.BUF		= buf;
-	driver_msg.CNT		= bytes;
-	driver_msg.PROC_NR	= proc_nr;
-	assert(dd_map[MAJOR(dev)].driver_nr != INVALID_DRIVER);
-	send_recv(BOTH, dd_map[MAJOR(dev)].driver_nr, &driver_msg);
+    driver_msg.type     = io_type;
+    driver_msg.DEVICE   = MINOR(dev);
+    driver_msg.POSITION = pos;
+    driver_msg.BUF      = buf;
+    driver_msg.CNT      = bytes;
+    driver_msg.PROC_NR  = proc_nr;
+    assert(dd_map[MAJOR(dev)].driver_nr != INVALID_DRIVER);
+    send_recv(BOTH, dd_map[MAJOR(dev)].driver_nr, &driver_msg);
 
-	return 0;
+    return 0;
 }
-
