2b4fa0a5986f1bd90daac8055d3b78696fe9be83
EricWang <wwssllabcd@gmail.com>
2016-04-22 15:51:21 +0800

    mod const

--- inc/proc.h
+++ inc/proc.h
@@ -91,7 +91,7 @@ struct task {
 /* Number of tasks & processes */
 #define NR_TASKS		4
 #define NR_PROCS		3
-#define NR_NATIVE_PROCS		4
+#define NR_NATIVE_PROCS		3
 #define FIRST_PROC		proc_table[0]
 #define LAST_PROC		proc_table[NR_TASKS + NR_PROCS - 1]
 
